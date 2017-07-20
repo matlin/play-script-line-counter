bb7d4855eab089c9009699c434bb7dcda6892608
Matthew Linkous <matthew.linkous@gmail.com>
2017-07-19 21:08:07 -0700

    Added real tests

--- test/parse.test.js
+++ test/parse.test.js
@@ -1,19 +1,28 @@
-import test from 'tape';
-import SampleScript from 'sample_script.txt';
-import ScriptParser from '../script-parser.js';
+const test = require("tape");
+const ScriptParser = require("../script-parser.js");
+const SampleScriptPath = "./sample_script.txt";
 
-test('Script Parser builds parser from parameters', (assert) => {
+test("Script Parser builds parser from parameters", assert => {
   const parser = new ScriptParser();
-  assert.equal(typeof parser, "function",
-    'Given two mismatched values, .equal() should produce a nice bug report');
+  assert.equal(
+    typeof parser,
+    "function",
+    "Given two mismatched values, .equal() should produce a nice bug report"
+  );
   assert.end();
 });
 
-test('The resulting parser returns an Object for valid XML', (assert) => {
+test("The resulting parser returns a a promise for a JS object when supplied valid XML file", assert => {
+  assert.plan(4); //plan for 4 assertions
   const parser = new ScriptParser();
-  const sample =
-
-  assert.equal(typeof parser, "function",
-    'Given two mismatched values, .equal() should produce a nice bug report');
-  assert.end();
+  const sample = parser(SampleScriptPath).then(obj => {
+    assert.equal(typeof obj, "object", "Should return a JS object when finished")
+  });
+  assert.equal(
+    typeof sample,
+    "object",
+    "The parser should be async and return a promise"
+  );
+  assert.true("then" in sample);
+  assert.true("catch" in sample);
 });
